114a1add97b86b0a32d4546b854de742f6a19de7
CLMNTDFR <deferclement59@gmail.com>
2024-02-20 19:05:56 +0100

    Adding readme

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,21 +9,26 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero", n);
-	}
-	else
-	{
-		printf("%d is negative", n);
-	}
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    
+    printf("%d ", n);
+
+    if (n > 0)
+    {
+        printf("is positive\n");
+    }
+    else if (n == 0)
+    {
+        printf("is zero\n");
+    }
+    else
+    {
+        printf("is negative\n");
+    }
+
+    return (0);
 }
+
