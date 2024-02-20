008be403768ee76afd06cc81d332f143f7e12f5f
CLMNTDFR <deferclement59@gmail.com>
2024-02-20 10:06:50 +0100

    first C program with put command

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a sentence
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+}
+	return (0);
+
