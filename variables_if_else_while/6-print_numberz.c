31790418ff83a69c06e78f77d2345acf757372ff
CLMNTDFR <deferclement59@gmail.com>
2024-02-21 10:40:52 +0100

    Program putchar print 0123456789

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number;
+
+	number = 0;
+	while (number <= 9)
+	{
+		putchar(number);
+		number++;
+	}
+	putchat('\n');
+
+	return (0);
+}
