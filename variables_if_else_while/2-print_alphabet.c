2f8ac2fa3a6e6a28e66ed4a80b186e6efb62802a
CLMNTDFR <deferclement59@gmail.com>
2024-02-21 09:37:32 +0100

    Program with putchar alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchat('\n');
+
+	return (0);
+}
