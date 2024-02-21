9d468b698707106e98a06dc322f995dc2aa604c9
CLMNTDFR <deferclement59@gmail.com>
2024-02-21 10:52:15 +0100

    Program print reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
