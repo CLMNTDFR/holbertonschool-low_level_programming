a4ea88b578ccd212a7a8ec7eb55668823b7a1e65
CLMNTDFR <deferclement59@gmail.com>
2024-02-21 09:50:25 +0100

    Program with putchar lower and upper alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	lower = 'a';
+	upper = 'A';
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+
+	return (0);
+}
