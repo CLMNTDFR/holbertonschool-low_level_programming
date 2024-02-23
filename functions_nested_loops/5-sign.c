6248243925b44a28d7b32341a3b09f63c2b97148
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 14:16:28 +0100

    Program that print if number is pos, neg or zero

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar(43);
+		return (-1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		return (0);
 		_putchar (48);
+		return (0);
 	}
 }
 
