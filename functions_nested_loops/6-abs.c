276885d0e233750a700897b58c879b29184777b9
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 15:16:36 +0100

    Program that print absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs -  function that computes the absolute value of an integer
+ * @r: the number to be compute
+ *
+ * Return: Absolute value of a number
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else if (r == 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r);
+	}
+}
+
