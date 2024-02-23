3f1154e769cd9af41f0f509aecf48ea58fdd36c2
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 21:49:47 +0100

    Program that print every minute of a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,19 +11,22 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
+					for (d = 0; d <= 9; d++)
+					{
 
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(58);
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
