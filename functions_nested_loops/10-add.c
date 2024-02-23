4809bb5719e565ffb918d3193c065b00af8bd198
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 16:25:27 +0100

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main;h"
+
+/**
+ * _add - function that adds two integers and return the result
+ * @n: value of the add
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b);
+{
+	int n = a + b;
+
+	_printf("%d\n", n);
+
+	return (0);
+}
