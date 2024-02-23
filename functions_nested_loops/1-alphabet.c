aa459b72d8acf7d3d945601af4ba5a0789aceab1
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 09:49:25 +0100

    Program print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet: Print alphabet in lower case followed by a new line
+ * print_alphabet - Print alphabet in lower case followed by a new line
  *
  * Return: void
  */
