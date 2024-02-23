3c7b1fc2cc166435365df447c4ff43b1d8a4a023
CLMNTDFR <deferclement59@gmail.com>
2024-02-23 11:23:14 +0100

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Check for lowercase character
+ * @c: the character to be checked
+ *
+ * Return: 1 for lower case or 0 for upper case
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+
