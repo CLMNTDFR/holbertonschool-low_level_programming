0a514b013e96274219fe9e142767777219dde8d8
CLMNTDFR <deferclement59@gmail.com>
2024-02-26 13:53:00 +0100

    Program that draws underscore n times

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in terminal
+ * @n: variable that is checked
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar(95);
+			a++;
+		}
+	}
+	else
+	{
+			_putchar(10);
+	}
+
+}
+
