0fae51167819a1d6080f1e1e7e279fd056a91d91
CLMNTDFR <deferclement59@gmail.com>
2024-02-26 11:52:55 +0100

    Program thatprints the number from 0 to 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers from 0 to 9 followed by a new line
+ * print_numbers - function that prints the numbers from 0 to 9
  *
  * Return: void
  */
