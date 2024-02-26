bd0df1a0206b0eb92071923052133fa526b74db6
CLMNTDFR <deferclement59@gmail.com>
2024-02-26 13:07:14 +0100

    Program that print 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 0 to 14, ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 1; y <= 10; y++)
+	{
+		for (x = 1; x <= 14; x++)
+		{
+			if (x > 9)
+			{
+				_putchar('1');
+			}
+			_putchar ('0' + (x % 10));
+		}
+		_putchar(10);
+	}
+}
+
