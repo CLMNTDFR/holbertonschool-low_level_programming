c0601f6211083d9df86af99b37e913608bee026d
CLMNTDFR <deferclement59@gmail.com>
2024-03-07 10:01:46 +0100

    program that prints the number of arguments passed into it

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,7 +10,10 @@
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	printf("%d\n", argc - 1);
-
+/** Nous ne voulons que le nombre d'arguments passés dans
+ * la commande, nous soustrayons 1 pour qu'il ignore son
+ * propre nom (qui est aussi un argument).
+ */
 	return (0);
 }
 
