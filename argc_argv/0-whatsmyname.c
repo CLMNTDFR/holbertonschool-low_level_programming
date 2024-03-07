ab5c71ff31f2420da9dbceda8242cb166fbbb8fb
CLMNTDFR <deferclement59@gmail.com>
2024-03-07 09:48:42 +0100

    program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Point Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
+
