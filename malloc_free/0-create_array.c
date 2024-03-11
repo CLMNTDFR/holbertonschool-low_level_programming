9a59da012783febbaf7eeb184a2b65547585064f
CLMNTDFR <deferclement59@gmail.com>
2024-03-11 16:06:52 +0100

    function that creates an array of chars, and initializes it with a specific char

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,29 +3,31 @@
 #include "main.h"
 
 /**
- * create_array - function that creates an array of chars, and initializes it
- *                with a specific char.
- *
- * @size: This is the length of the array
- * @c: This is the input character
- *
- * Return: An Array Initialized with the specific char
- */
+* create_array - function that creates an array of chars, and initializes it
+*                with a specific char.
+*
+* @size: This is the length of the array
+* @c: This is the input character
+*
+* Return: An Array Initialized with the specific char
+*/
 char *create_array(unsigned int size, char c)
 {
-    unsigned int index;
-    char *s;
+	unsigned int index;
+
+	char *s;
+
 /* *s pointeur vers le début de l'array alloué dynamiquement */
 
-    s = (char *) malloc(size * sizeof(char));
-/* la fonction malloc renvoie un pointeur vers le début de la mémoire allouée */
-    if (s == NULL)
-        return (NULL);
+	s = (char *) malloc(size * sizeof(char));
+/* malloc renvoie un pointeur vers le début de la mémoire allouée */
+	if (s == NULL)
+		return (NULL);
 /* Vérifie si l'allocation a échoué */
-    for (index = 0; index < size; index++)
-    {
-        s[index] = c;
+	for (index = 0; index < size; index++)
+	{
+		s[index] = c;
 /* Chaque élement du tableau est initialisé avec le caractère "c" */
-    }
-    return (s);
+	}
+	return (s);
 }
