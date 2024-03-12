fe014906e1b969efda6d08c036464a7f8104479f
CLMNTDFR <deferclement59@gmail.com>
2024-03-12 15:25:25 +0100

    function that allocates memory using malloc and exit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* malloc_checked -  function that allocates memory using malloc
+* @b: This is the length to allocate in the memory
+*
+* Return: Returns a pointer to the allocated memory
+* if malloc fails, the malloc_checked function should cause normal
+* process termination with a status value of 98
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+/* pointeur générique vers n'importe quel type de donné */
+/* si besoin, le pointeur sera "casté" vers un autre type de donné */
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+/* arrête l'exe du programme et renvoie le code de statut 98 */
+	return (p);
+}
