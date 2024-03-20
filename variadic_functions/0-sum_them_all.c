14204fd4217e8b63e87d56614477a345591bd438
CLMNTDFR <deferclement59@gmail.com>
2024-03-20 11:14:24 +0100

    function that returns the sum of all its parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,22 +11,23 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list liste;
+	va_list liste; /* Variable stockant la liste d'arg */
 
 	int sum = 0;
 
-	unsigned int i;
+	unsigned int i; /* index unsigned car comparaison avec n unsigned */
 
 	if (n == 0)
-	return (0);
+	return (0); /* Gestion des cas d'échec */
 
 	va_start(liste, n);
+    /* va_start prend le nom de la va_liste et la dernière variable connue */
 	for (i = 0; i < n; i++)
 	{
 		sum = sum + va_arg(liste, unsigned int);
 
 	}
-	va_end(liste);
+	va_end(liste); /* Libération mémoire */
 
 	return (sum);
 }
