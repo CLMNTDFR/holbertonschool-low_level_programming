45326368bf5f3ca9059d4674e1637a2b378af33d
CLMNTDFR <deferclement59@gmail.com>
2024-04-02 14:26:19 +0200

    update lists.h with the prototype of _strlen

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _strlen(const char *string);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
