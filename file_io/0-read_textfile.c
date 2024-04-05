f3927db5b65fa11f7444ef8a9c50a196f1d5c760
CLMNTDFR <deferclement59@gmail.com>
2024-04-05 17:15:54 +0200

    function that reads a text file and prints it to the POSIX standard output

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,45 +1,55 @@
 #include "main.h"
 
 /**
-* read_textfile- read a text file and print it
+* read_textfile - read a text file and print it
 * @filename: Name of the file
-* @letters: Nuber of letter it should read and print
-* Return: the number of letters read and print
+* @letters: Number of letters it should read and print
+* Return: the number of letters read and printed
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filedescriptor;
-	int reading;
-	ssize_t writing;
-	char *buffer;
+    int filedescriptor;
+    int reading;
+    ssize_t writing;
+    char *buffer;
 
-	if (filename == NULL)
-	return (0);
+    /* Check if filename is NULL */
+    if (filename == NULL)
+        return (0);
 
-	filedescriptor = open(filename, O_RDONLY);
-	if (filedescriptor == -1)
-	return (0);
+    /* Open the file for reading */
+    filedescriptor = open(filename, O_RDONLY);
+    if (filedescriptor == -1)
+        return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	return (0);
+    /* Allocate memory for the buffer */
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL) {
+        close(filedescriptor);
+        return (0);
+    }
 
-	reading = read(filedescriptor, buffer, letters);
-	if (reading == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+    /* Read data from the file into the buffer */
+    reading = read(filedescriptor, buffer, letters);
+    if (reading == -1) {
+        free(buffer);
+        close(filedescriptor);
+        return (0);
+    }
 
-	writing = write(STDOUT_FILENO, buffer, reading);
-	if (writing == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+    /* Write data from the buffer to STDOUT */
+    writing = write(STDOUT_FILENO, buffer, reading);
+    if (writing == -1 || writing < reading) {
+        free(buffer);
+        close(filedescriptor);
+        return (0);
+    }
 
-	free(buffer);
-	close(filedescriptor);
-	return (writing);
+    /* Free the memory and close the file */
+    free(buffer);
+    close(filedescriptor);
+
+    /* Return the number of letters read and printed */
+    return (writing);
 }
