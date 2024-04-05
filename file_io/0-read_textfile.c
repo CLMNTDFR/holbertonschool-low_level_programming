2bd0dedb66821b154a8635611c4c110b68cc3051
CLMNTDFR <deferclement59@gmail.com>
2024-04-05 17:11:14 +0200

    function that reads a text file and prints it to the POSIX standard output

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,57 +1,45 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd>
 
 /**
-* read_textfile - Reads a text file and prints it to POSIX stdout
-* @filename: a pointer to the name of the file
-* @letters: the number of letters the function should read & print
-*
-* Return: * Return: 0 if function fails of if filename is NULL
-*         O/w - the actual number of bytes the function car read and print
+* read_textfile- read a text file and print it
+* @filename: Name of the file
+* @letters: Nuber of letter it should read and print
+* Return: the number of letters read and print
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w; /* ssize_t = signed size_t */
-	char *buffer; /* fr: tampon */
+	int filedescriptor;
+	int reading;
+	ssize_t writing;
+	char *buffer;
 
-/* Check if filename is NULL */
 	if (filename == NULL)
-		return (0);
+	return (0);
 
-/* Allocate memory for the buffer et check success */
+	filedescriptor = open(filename, O_RDONLY);
+	if (filedescriptor == -1)
+	return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
+	return (0);
 
-/* open file in RDONLY */
-	o = open(filename, O_RDONLY);
-	if (o == -1)
-    {
+	reading = read(filedescriptor, buffer, letters);
+	if (reading == -1)
+	{
 		free(buffer);
 		return (0);
 	}
-/* read 'letters' bytes of data in buffer */
-	r = read(o, buffer, letters);
-	if (r == -1)
-    {
+
+	writing = write(STDOUT_FILENO, buffer, reading);
+	if (writing == -1)
+	{
 		free(buffer);
-		close(o);
 		return (0);
 	}
-/* write buffer's data on std output POSIX */
-/* 'r' represent number of readed datas */
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || w < r) {
-		free(buffer);
-		close(o);
-		return (0);
-	}
-/* check if error when program open, read or write */
-/* or if r's data and w's data have different values */
 
 	free(buffer);
-	close(o);
-
-	return (w);
+	close(filedescriptor);
+	return (writing);
 }
